b1be816c5da45bc2c8f252194110c57a4fca209f
ManoelFernando98 <manoelfernandoconceicao@gmail.com>
2020-04-12 10:35:21 -0300

    Teste
    Testando

---
+++ Assets/sc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc : MonoBehaviour
+{
+    public float speed1 = 1f;
+    private Rigidbody2D rig1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig1 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+
+            rig1.velocity = Vector2.up * speed1;
+            
+        }
+        
+    }
+}
